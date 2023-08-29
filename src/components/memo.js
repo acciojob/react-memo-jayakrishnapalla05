d8a8af01d9d7adb28612f37e5a13be648daf7535
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-29 11:35:46 +0530

    submitting code-Tue Aug 29 2023 11:35:45 GMT+0530 (India Standard Time)

--- src/components/memo.js
+++ src/components/memo.js
@@ -11,7 +11,7 @@ const memo=()=>{
         <div>
             <h2>React.Memo</h2>
            
-            <input type="skill-input" type="text" onChange={(e)=>setSkill(e.target.value)}></input>
+            <input id="skill-input" type="text" onChange={(e)=>setSkill(e.target.value)}></input>
             <button type="button" id="skill-btn" onClick={()=>setMemo([...memo,skill])}>Add Skill</button>
             <br/>
             {
